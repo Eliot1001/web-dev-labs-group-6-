eb10815445bc7867aebc6bd3f81bbd1713083f66
Eliot1001 <eliotjoseph487@gmail.com>
2026-07-21 21:20:05 +0300

    Update week 6 Django views and templates

--- week 6/pop_system/urls.py
+++ week 6/pop_system/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from pop_system import views
 
 urlpatterns = [
-    path("hello/", views.index)
+    path("", views.home, name="home"),
+    path("proposals/", views.proposal_list, name="proposal_list"),
 ]
--- week 6/pop_system/views.py
+++ week 6/pop_system/views.py
@@ -1,6 +1,40 @@
+from django.db.utils import OperationalError, ProgrammingError
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Project Proposal Management System")
+from .models import Proposal, Review, User
+
+
+def home(request):
+    stats = {
+        "users": 0,
+        "proposals": 0,
+        "reviews": 0,
+    }
+    recent_proposals = []
+
+    try:
+        stats = {
+            "users": User.objects.count(),
+            "proposals": Proposal.objects.count(),
+            "reviews": Review.objects.count(),
+        }
+        recent_proposals = Proposal.objects.select_related("user").order_by("-submission")[:5]
+    except (OperationalError, ProgrammingError):
+        pass
+
+    context = {
+        "stats": stats,
+        "recent_proposals": recent_proposals,
+    }
+    return render(request, "pop_system/home.html", context)
+
+
+def proposal_list(request):
+    proposals = []
+
+    try:
+        proposals = Proposal.objects.select_related("user").order_by("-submission")
+    except (OperationalError, ProgrammingError):
+        pass
+
+    return render(request, "pop_system/proposal_list.html", {"proposals": proposals})
